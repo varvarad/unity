b5d63e3b8c71acc4f9b399c458a901e81e4deb0b
Varya V <varvarad.vlasova@gmail.com>
2019-04-12 20:13:46 +0300

    Error hw6

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float distance = 0.01f;
+   
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name=="Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        //transform.position = Vector3.Lerp(transform.position, distance * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,7 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject CubePrefab;
     Vector3 Position;
-    public int Counter = 100;
+    public int Counter = 1000;
     int count;
 
     void Start()
@@ -17,16 +17,11 @@ public class Spawner : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
             if (count < Counter)
             {
                 Position = new Vector3(Random.Range(60, 305), transform.position.y, Random.Range(50, 270));
                 Instantiate(CubePrefab, Position, Quaternion.identity);
                 count++;
             }
-        }
-        
-        
     }
 }
